230cc4f62363bccd2945bd551cf68b13e353f34f
Sandro Domitran <sdomitran@gmail.com>
2019-07-18 10:36:00 +0200

    Homework4

--- TVShows/TVShows/Login/LoginViewController.swift
+++ TVShows/TVShows/Login/LoginViewController.swift
@@ -7,12 +7,58 @@
 //
 
 import UIKit
+import Alamofire
+import CodableAlamofire
+import SVProgressHUD
+
+struct User: Codable {
+    let email: String
+    let type: String
+    let id: String
+    enum CodingKeys: String, CodingKey {
+        case email
+        case type
+        case id = "_id"
+    }
+}
+
+struct LoginUser: Codable {
+    let token: String
+}
+
+
+let parameters: [String: String] = [
+    "email": "",
+    "password": ""
+]
 
 class LoginViewController: UIViewController {
     
+    @IBOutlet private weak var usernameTextField: UITextField!
+    @IBOutlet private weak var passwordTextField: UITextField!
     @IBOutlet private weak var loginButton: UIButton!
-    @IBOutlet private weak var createAccountButton: UIButton!
-    @IBOutlet weak var rememberButton: UIButton!
+    @IBOutlet private weak var registerButton: UIButton!
+    @IBOutlet private weak var rememberButton: UIButton!
+    
+    private var newUser: User?
+    private var loginUser: LoginUser?
+    
+    
+    private var username : String {
+        if let text = self.usernameTextField.text {
+            return text
+        } else{
+            return ""
+        }
+    }
+    
+    private var password : String {
+        if let text = self.passwordTextField.text {
+            return text
+        } else{
+            return ""
+        }
+    }
     
     private lazy var rememberCheckBox = CheckBox(checkedImage: "ic-checkbox-filled", uncheckedImage: "ic-checkbox-empty", button: self.rememberButton)
  
@@ -29,11 +75,121 @@ class LoginViewController: UIViewController {
         super.viewDidLoad()
         loginButton.layer.cornerRadius = 10
         loginButton.backgroundColor = pink
-        createAccountButton.setTitleColor(pink, for: .normal)
+        registerButton.setTitleColor(pink, for: .normal)
     }
 
-    @IBAction func rememberButtonPressed(_ sender: UIButton) {
+    @IBAction private func rememberButtonPressed(_ sender: UIButton) {
         rememberCheckBox.pressed()
     }
+    
+    @IBAction private func loginButtonPressed(_ sender: Any) {
+        if checkUsernameAndPassword() {
+            
+                SVProgressHUD.show()
+                let parameters: [String: String] = [
+                    "email": username,
+                    "password": password
+                ]
+                
+                Alamofire
+                    .request("https://api.infinum.academy/api/users/sessions",
+                             method: .post,
+                             parameters: parameters,
+                             encoding: JSONEncoding.default)
+                    .validate()
+                    .responseDecodableObject(keyPath: "data", decoder: JSONDecoder()) {
+                        (response: DataResponse<LoginUser>) in
+                        
+                        SVProgressHUD.dismiss()
+                        switch response.result {
+                        case .success(let user):
+                            self.loginUser = user
+                            let storyboard = UIStoryboard(name: "Home", bundle: nil)
+                            let vc = storyboard.instantiateViewController(withIdentifier: "HomeViewController") as UIViewController
+                            self.show(vc, sender: self)
+                        case .failure(let error):
+                            self.unableToLogin()
+                            print("API failure: \(error)")
+                        }
+                        
+                    }
+            }
+    }
+    
+    @IBAction private func registerButtonPressed(_ sender: Any) {
+        if checkUsernameAndPassword() {
+            
+            SVProgressHUD.show()
+            
+            let parameters: [String: String] = [
+                "email": username,
+                "password": password
+            ]
+            
+            Alamofire
+                .request("https://api.infinum.academy/api/users",
+                         method: .post,
+                         parameters: parameters,
+                         encoding: JSONEncoding.default)
+                .validate()
+                .responseDecodableObject(keyPath: "data", decoder: JSONDecoder()) {
+                    (response: DataResponse<User>) in
+                    
+                    SVProgressHUD.dismiss()
+                    switch response.result {
+                    case .success(let user):
+                        self.newUser = user
+                        let storyboard = UIStoryboard(name: "Home", bundle: nil)
+                        let vc = storyboard.instantiateViewController(withIdentifier: "HomeViewController") as UIViewController
+                        self.show(vc, sender: self)
+                    case .failure(let error):
+                        self.unableToRegister()
+                        print("API failure: \(error)")
+                    }
+                    
+            }
+        }
+    }
+    
 
+    private func unableToLogin() {
+        let alert = UIAlertController(
+            title: "Unable to login",
+            message: "Please check username and password",
+            preferredStyle: UIAlertController.Style.alert)
+        
+        let OKAction = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(OKAction)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    private func unableToRegister() {
+        let alert = UIAlertController(
+            title: "Unable to register",
+            message: "Please check entered email",
+            preferredStyle: UIAlertController.Style.alert)
+        
+        let OKAction = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(OKAction)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    
+    private func checkUsernameAndPassword() -> Bool {
+        if username.isEmpty || password.isEmpty {
+            let alert = UIAlertController(
+                title: "Empty Fields",
+                message: "Please fill username and password",
+                preferredStyle: UIAlertController.Style.alert)
+            
+            let OKAction = UIAlertAction(title: "OK", style: .default)
+            alert.addAction(OKAction)
+            
+            present(alert, animated: true, completion: nil)
+            return false
+        }
+        return true
+    }
 }
